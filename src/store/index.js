f9e24f8abdc896eacdfa3369927ac5c614a89096
Jacek Karczmarczyk <jk@drukcd.pl>
2018-08-13 23:11:37 +0200

    @mdi/font

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import __global__ from './__global__'
 import initPersistentState from './storagePlugin'
 import { name as storageKey } from '../../package.json'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuex)
 
