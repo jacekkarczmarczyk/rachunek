064166f5e93a5d20bb225999fd54924379315bd0
Jacek Karczmarczyk <jkarczm@gmail.com>
2022-01-19 15:24:25 +0100

    refactor: vite

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin } from 'vite-plugin-vue2';
+import { resolve } from 'path';
+import Components from 'unplugin-vue-components/vite';
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
+import checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+// https://github.com/logue/vite-vue2-vuetify-ts-starter/blob/master/vite.config.ts
+export default defineConfig({
+  plugins: [
+    createVuePlugin(),
+    // Vuetify
+    Components({
+      // generate `components.d.ts` global declarations
+      // dts: true,
+      // auto import for directives
+      directives: false,
+      // resolvers for custom components
+      resolvers: [VuetifyResolver()],
+    }),
+    checker({
+      typescript: true,
+      eslint: {
+        files: ['./src'],
+        extensions: ['.ts', '.js', '.vue'],
+      },
+      overlay: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+});
